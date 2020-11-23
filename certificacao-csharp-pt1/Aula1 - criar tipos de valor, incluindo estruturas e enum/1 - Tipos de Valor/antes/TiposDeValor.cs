8756ce93eb16ed29e6443b5695ad3f3efe779c96
Julio Pinheiro <juliorenan@live.com>
2020-11-23 20:36:14 -0300

    initial commit

---
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/1 - Tipos de Valor/antes/TiposDeValor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro.antes
+{
+    class TiposDeValor : IAulaItem
+    {
+        public void Executar()
+        {
+            int idade = 18;
+            Console.WriteLine(idade);
+        }
+    }
+}
---
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/1 - Tipos de Valor/depois/TiposDeValor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class TiposDeValor : IAulaItem
+    {
+        public void Executar()
+        {
+            int idade = 28;
+            Console.WriteLine(idade);
+
+            int idade2 = idade;
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"idade2: {idade2}");
+        }
+    }
+}
