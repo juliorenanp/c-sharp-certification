83a02526d97671f8a2880d4eeff7ccef67bd3435
Julio Pinheiro <juliorenan@live.com>
2020-11-25 21:31:42 -0300

    feat: Added new types of variables

--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/2 - Tipos Inteiros/depois/Inteiros.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/2 - Tipos Inteiros/depois/Inteiros.cs
@@ -11,6 +11,14 @@ namespace certificacao_csharp_roteiro
         public void Executar()
         {
             int? inteiro = null;
+            byte tipoDeAzul = 0xFF; // = 255 em decimal;
+            short pessoasEsperandoBleach = 230; // System.Int16
+            long populacaoBrasil = 207_660_900; // 207kk
+            sbyte nivelBrilho = -17; // System.Sbyte
+            ushort semSinal = 100; // System.UInt16 
+            uint mangas = 11; // System.UInt32
+            ulong episodiosNaruto = 8_000_000_000;// System.UInt32
+
         }
     }
 }
