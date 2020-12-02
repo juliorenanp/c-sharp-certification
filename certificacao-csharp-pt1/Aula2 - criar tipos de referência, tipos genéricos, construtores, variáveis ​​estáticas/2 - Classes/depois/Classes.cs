6adcebd5a7d367b0f05d1bee982c03414fe38c55
Julio Pinheiro <juliorenan@live.com>
2020-12-02 20:28:37 -0300

    feat: added OOP example

--- certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/2 - Classes/depois/Classes.cs
+++ certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/2 - Classes/depois/Classes.cs
@@ -10,15 +10,41 @@ namespace certificacao_csharp_roteiro
     {
         public void Executar()
         {
-
+            
         }
 
      // pode ser vista dentro de mesmo assembly, neste caso a certificacao-charp-pt1
-     internal class Humano
+     internal class SerVivo
         {
+            public SerVivo(int idade, string nome, int peso)
+            {
+                Idade = idade;
+                Nome = nome;
+                Peso = peso;
+            }
+
             public int Idade { get; set; }
             public string Nome { get; set; }
             public int Peso { get; set; }
+
+            protected int retornaIdade()
+            {
+                return this.Idade;
+            }
+
+            public int retornaIdadePub()
+            {
+                return this.Idade;
+            }
+
+        }
+
+        private class Humano : SerVivo
+        {
+            public Humano(int idade, string nome, int peso) : base(idade, nome, peso)
+            {
+
+            }
         }
     }
 }
