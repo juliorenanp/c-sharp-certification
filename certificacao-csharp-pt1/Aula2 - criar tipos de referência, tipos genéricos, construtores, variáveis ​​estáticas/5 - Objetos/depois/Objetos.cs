f3e75d7a02d2109ba36b2b16152e7ca460d6c7b6
Julio Pinheiro <juliorenan@live.com>
2020-12-02 20:56:52 -0300

    feat:  added some Object examples

--- certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/5 - Objetos/depois/Objetos.cs
+++ certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/5 - Objetos/depois/Objetos.cs
@@ -12,6 +12,26 @@ namespace certificacao_csharp_roteiro
         {
             int pontuacao = 10;
             Console.WriteLine($"pontuacao: {pontuacao}");
+
+            Console.WriteLine($"Object com valor primitivo");
+            object meuObjeto;
+            meuObjeto = pontuacao;
+            Console.WriteLine($"meuObjeto{meuObjeto}");
+            Console.WriteLine($"meuObjeto{meuObjeto.GetType()}");
+
+
+            Console.WriteLine();
+            Console.WriteLine("Object com referência de objeto");
+
+            meuObjeto = new Jogador();
+            //referencia de classe para Jogador
+            Jogador classRef;
+            
+            // exemplo de conversão explicita ou Cast
+            classRef = (Jogador)meuObjeto;
+
+            Console.WriteLine($"classRef.Pontuacao: {classRef.Pontuacao}");
+
         }
     }
 
