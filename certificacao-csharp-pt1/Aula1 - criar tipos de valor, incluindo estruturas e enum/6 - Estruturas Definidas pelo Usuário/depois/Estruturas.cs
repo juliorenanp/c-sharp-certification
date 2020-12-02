0c02dd6416c117199f48fb66825613eceba12f0d
Julio Pinheiro <juliorenan@live.com>
2020-12-01 21:01:38 -0300

    feat: Added new csharp types 🍕🍕�

--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/6 - Estruturas Definidas pelo Usuário/depois/Estruturas.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/6 - Estruturas Definidas pelo Usuário/depois/Estruturas.cs
@@ -18,6 +18,26 @@ namespace certificacao_csharp_roteiro
             Console.WriteLine($"Longitude1 = {Longitude1}");
             Console.WriteLine($"Latitude2 = {Latitude2}");
             Console.WriteLine($"Longitude2 = {Longitude2}");
+
+            // By default all variables created as struct are created with 0 (for double).
+            PosicaoGps posicao1;
+            posicao1.Latitude = 15.23;
+            posicao1.Longitude = 29.30;
+
+            posicao1 = new PosicaoGps() { Longitude = 33.12, Latitude = 12.33 };
+            posicao1 = new PosicaoGps(32.11, 33.52);
+        }
+    }
+
+    struct PosicaoGps
+    {
+        public double Latitude;
+        public double Longitude;
+
+        public PosicaoGps(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
         }
     }
 }
